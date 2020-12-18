eff9c24205e8e598cd1db617f18d6e41f857dbcb
Joao Paulo <jobis_92@hotmail.com>
2020-12-18 19:26:00 -0300

    Listando todas as Categorias com DTO

--- src/main/java/com/joaopaulo/cursomc/services/CategoriaService.java
+++ src/main/java/com/joaopaulo/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.joaopaulo.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -47,5 +48,9 @@ public class CategoriaService {
 		
 		
 	}
-	
+	 public List<Categoria> findAll(){
+		 
+		 return repo.findAll();
+		 
+	 }
 }
